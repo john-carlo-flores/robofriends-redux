1eabd68d63eae1426ad578b9a43843bdc4331620
JC Flores <jn.carlo.flores@esgglobal.com>
2025-12-03 11:37:24 -0700

    Update service worker

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { createLogger } from 'redux-logger';
 import 'tachyons';
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
@@ -35,5 +35,9 @@ root.render(
 //   </Provider>,
 //   document.getElementById('root')
 // );
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
